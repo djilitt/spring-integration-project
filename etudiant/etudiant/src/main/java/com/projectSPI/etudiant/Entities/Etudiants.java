862d52ea34fc49a1a00f68099eaedb59e4ea8905
djilit <21047@supnum.mr>
2023-12-13 18:38:28 +0000

    students attribut added

--- etudiant/etudiant/src/main/java/com/projectSPI/etudiant/Entities/Etudiants.java
+++ etudiant/etudiant/src/main/java/com/projectSPI/etudiant/Entities/Etudiants.java
@@ -1,5 +1,15 @@
 package com.projectSPI.etudiant.Entities;
 
+import java.util.Date;
+
 public class Etudiants {
+
+    private Long id;
+    private String prenom;
+    private String nom;
+    private String Matricule;
+
+    private String Departement ;
+
     
 }
