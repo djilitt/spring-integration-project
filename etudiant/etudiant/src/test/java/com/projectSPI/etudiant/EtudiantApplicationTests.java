f2e013a0ad54ea5ed4c0b0fb12b940f3fb96c4b3
ba ablaye <21040@supnum.mr>
2023-12-13 16:52:06 -0800

    ajout des projet

---
+++ etudiant/etudiant/src/test/java/com/projectSPI/etudiant/EtudiantApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projectSPI.etudiant;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EtudiantApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
